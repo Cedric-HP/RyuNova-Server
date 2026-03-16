22e01aa8db622cd3eaaffcfef323da7701574837
Cédric <cedricmouchon23hp@gmail.com>
2026-03-16 16:38:43 +0100

    2nd Attempt to fix DB issues

--- db.js
+++ db.js
@@ -3,13 +3,15 @@ require("dotenv").config();
 
 // Creation of the connection to the data base
 const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+    process.env.DATABASE_URL, {
         dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 module.exports = sequelize
